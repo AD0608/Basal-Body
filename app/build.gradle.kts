71c1fe2e9907f3b3563fe27ced332caf519751e7
AD0608 <ajaymakwana0608@gmail.com>
2025-09-09 09:37:34 +0530

    Fixed crash issue

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,13 +72,13 @@ android {
     productFlavors {
         create("development") {
             dimension = "MXB"
-            buildConfigField("String", "BASE_URL", "\"www.google.com\"")
-            buildConfigField("String", "SOCKET_BASE_URL", "\"www.google.com\"")
+            buildConfigField("String", "BASE_URL", "\"https://www.google.com\"")
+            buildConfigField("String", "SOCKET_BASE_URL", "\"https://www.google.com\"")
         }
         create("production") {
             dimension = "MXB"
-            buildConfigField("String", "BASE_URL", "\"www.google.com\"")
-            buildConfigField("String", "SOCKET_BASE_URL", "\"www.google.com\"")
+            buildConfigField("String", "BASE_URL", "\"https://www.google.com\"")
+            buildConfigField("String", "SOCKET_BASE_URL", "\"https://www.google.com\"")
         }
     }
 }
